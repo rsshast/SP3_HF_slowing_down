81cf516b9de9d7ecfa87ad2c2d85adeccf6e3856
Ravi Shastri <rsshast@umich.edu>
2025-08-26 11:43:43 -0400

    diagonal xs's handled properly

--- batch_sp3.sh
+++ batch_sp3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=sp3_slowing_down
+#SBATCH --job-name=sp3
 #SBATCH --mail-type=All
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=36
 #SBATCH --mem=50G
-#SBATCH --time=48:00:00
+#SBATCH --time=00:30:00
 #SBATCH --account=bckiedro0
 #SBATCH --partition=standard
 #SBATCH --export=ALL
